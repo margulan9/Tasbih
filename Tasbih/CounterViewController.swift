b0d86f82e267e244af4c4e97542a3c06f559b4f2
margulan9 <sugirbaymargulan@gmail.com>
2020-04-15 11:12:18 +0600

    add SnapKit

--- Tasbih/CounterViewController.swift
+++ Tasbih/CounterViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  CounterViewController.swift
 //  Tasbih
 //
 //  Created by Sugirbay Margulan on 4/15/20.
@@ -8,13 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CounterViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        view.backgroundColor = .lightGray
     }
+    
 
 
 }
-
