0e454a77485ef81c64dd091ff323122313f17835
margulan9 <sugirbaymargulan@gmail.com>
2020-04-15 11:03:11 +0600

    Initial Commit

---
+++ Tasbih/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tasbih
+//
+//  Created by Sugirbay Margulan on 4/15/20.
+//  Copyright © 2020 Sugirbay Margulan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
